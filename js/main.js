4e5af3b651d53e24295e5144c960daf3ff1e0ceb
Konstantin Teryohin <TeryohinKI@rubius.rubius.com>
2020-10-19 09:19:45 +0700

    some fixes

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(function () {
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1
-  });  
+  });
 
   $('input.form-control, textarea.form-control').floatingLabel();
 
@@ -33,13 +33,13 @@ $(function () {
     mask: '+7 (999) 999-99-99'
   });
 
-  $('.mobile-btn').click(function() {
+  $('.mobile-btn').click(function () {
     $(this).toggleClass('active');
     $('.mobile-menu').toggleClass('active');
     $('body').toggleClass('no-scroll');
   });
 
-  $('form#service-form, form#order-product-form, form#order-form').validate({
+  $('form#order-form').validate({
     rules: {
       name: 'required',
       phone: {
@@ -52,5 +52,49 @@ $(function () {
     submitHandler: function (form) {
       form.submit();
     }
+  });
+
+  $('form#service-form').validate({
+    rules: {
+      name: 'required',
+      phone: {
+        required: true,
+        phoneValidator: true
+      },
+      policy: 'required'
+    },
+    errorPlacement: function (error, element) { },
+    submitHandler: function (form) {
+      form.submit();
+    }
+  });
+
+  $('form#order-product-form').validate({
+    rules: {
+      name: 'required',
+      phone: {
+        required: true,
+        phoneValidator: true
+      },
+      policy: 'required'
+    },
+    errorPlacement: function (error, element) { },
+    submitHandler: function (form) {
+      form.submit();
+    }
+  });
+
+  $("button[data-type=order]").click(function () {
+    let target = $(this).data('target');
+
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(`#${target}`).offset().top
+    }, 1000);
+  });
+
+  $('.product__sample').click(function() {
+    $(this).toggleClass('active');
+    let check = $('#sample-checkbox');
+    check.prop('checked', !check.prop('checked'));
   });
 });
